c69f175fe8448c268580ab13b6cd625b0b2299f5
aboxnz <alan@abox.co.nz>
2016-05-29 14:39:19 +1200

    added a subtraction function and button

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function sayVal() {
+function addition() {
 		var input1 = parseInt(document.getElementById("in1").value);
 		var input2 = parseInt(document.getElementById("in2").value);
 	  function add(value1, value2) {
 	    return (value1 + value2)
 	  }
-	  var sum = add(input1,input2); 
-		document.getElementById("result").value = sum;
+	  var total = add(input1,input2); 
+		document.getElementById("result").value = total;
+	}
+
+function subtraction() {
+		var input1 = parseInt(document.getElementById("in1").value);
+		var input2 = parseInt(document.getElementById("in2").value);
+	  function sub(value1, value2) {
+	    return (value1 - value2)
+	  }
+	  var total = sub(input1,input2); 
+		document.getElementById("result").value = total;
 	}
