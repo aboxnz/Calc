69a66b3dcc1452db25eec68f30957f202e4b9f71
aboxnz <alan@abox.co.nz>
2016-05-29 22:21:41 +1200

    reduce code

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-function addition() {
-		var input1 = parseInt(document.getElementById("in1").value);
-		var input2 = parseInt(document.getElementById("in2").value);
-	  function add(value1, value2) {
-	    return (value1 + value2)
-	  }
-	  var total = add(input1,input2); 
-		document.getElementById("result").value = total;
-	}
-
-function subtraction() {
-		var input1 = parseInt(document.getElementById("in1").value);
-		var input2 = parseInt(document.getElementById("in2").value);
-	  function sub(value1, value2) {
-	    return (value1 - value2)
-	  }
-	  var total = sub(input1,input2); 
-		document.getElementById("result").value = total;
-	}
+function checkButton(id) {
+  var whichButton = id;
+  var input1 = parseInt(document.getElementById("in1").value);
+  var input2 = parseInt(document.getElementById("in2").value);
+  if (whichButton === "addition") {
+    document.getElementById("result").value = input1 + input2;
+  } 
+  else if (whichButton === "subtraction") {
+    document.getElementById("result").value = input1 - input2;
+  }
+};
